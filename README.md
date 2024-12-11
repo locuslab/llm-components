c1020cc67749508ca92f48d61b2db98d2da7f2fa
Eric-mingjie <smj690308>
2024-12-11 07:58:26 -0500

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+## Commands
+
+collect mean ablation for one attention head:
+```sh
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --model llama3.1-8b-it \
+    --head_str L0H0 \
+    --collect_mean \
+    --savedir results/llama3.1-8b-it/mean_ablate/
+```
+After this step, "mean_outputs_100.pt" will be stored for the mean outputs of L0H0 attn head over 100 sequences.
+
+Then we use this ablation to get the token-wise loss for mean ablation one attention head, e.g., L0H0 (layer 0 attn head 0).
+```sh
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --model llama3.1-8b-it \
+    --head_str L0H0 \
+    --savedir results/llama3.1-8b-it/mean_ablate/
+```
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 import numpy as np
 import re 
 from datasets import load_dataset
-
+from tqdm import tqdm
 
 from pdb import set_trace as st 
 
@@ -36,13 +36,39 @@ def get_loss(model, tokenizer, prompt, device = torch.device("cuda:0")):
     loss = F.cross_entropy(logits, labels, reduction="none")
     return loss 
 
-# def collect_mean_ablation(model, tokenizer, prompt, device = torch.device("cuda:0")):
-#     pass 
+def collect_mean_ablation(args, model, tokenizer, device = torch.device("cuda:0")):
+    ds = load_dataset("lmsys/lmsys-chat-1m", cache_dir="/data/locus/project_data/project_data3/mingjies/huggingface")
+
+    layers = model.model.layers
+    ### collect mean head stats
+    for layer_id in range(len(layers)):
+        mp.mean_ablate_llama3_attn_head(layers[layer_id], 0, collect_stats=True)
+
+    #### code for zero ablation 
+    for i in tqdm(range(args.num_samples)):
+        prompt = ds["train"][i]["conversation"][0]["content"]
+        get_loss(model, tokenizer, prompt)
+
+    outputs = []
+    for layer_id in range(len(layers)):
+        mean_attn_outputs = layers[layer_id].self_attn.mean_head_stats
+        outputs.append(mean_attn_outputs)
+    torch.save(outputs, os.path.join(args.savedir, f"mean_outputs_{args.num_samples}.pt"))
+
+def analyze():
+    obj_default = torch.load("results/llama3.1-8b-it/mean_ablate/token_agnostic/no_ablate.pt")
+    obj_mean = torch.load("results/llama3.1-8b-it/mean_ablate/token_agnostic/mean_ablate_L0H0.pt")
+
+    # for i in range(len(obj_default)):
+    #     pass 
+    exit()
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, help='LLaMA model')
     parser.add_argument("--head_str", type=str, default="L0H0")
+    parser.add_argument("--num_samples", type=int, default=100)
+    parser.add_argument("--collect_mean", action="store_true")
     parser.add_argument("--access_token", type=str, default="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")
     parser.add_argument("--savedir", type=str, default=".")
     args = parser.parse_args()
@@ -56,17 +82,26 @@ def main():
 
     layer_id, attn_id = extract_head_info(args.head_str)
 
+    if args.collect_mean:
+        collect_mean_ablation(args, model, tokenizer)
+        return
+    # analyze()
+
     # prompt = "Janet’s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?"
     ds = load_dataset("lmsys/lmsys-chat-1m", cache_dir="/data/locus/project_data/project_data3/mingjies/huggingface")
 
-    #### code for zero ablation 
-    for i in range(5):
-        prompt = ds["train"][i]["conversation"][0]["content"]
-        loss1 = get_loss(model, tokenizer, prompt)
-        mp.zero_ablate_llama3_attn_head(layers[layer_id], attn_id)
-        loss2 = get_loss(model, tokenizer, prompt)
+    #### mean ablate llama3 attn_head;
+    obj = torch.load("results/llama3.1-8b-it/mean_ablate/token_agnostic/mean_outputs_100.pt")
+    layers[layer_id].self_attn.mean_head_stats = obj[layer_id]
+    mp.mean_ablate_llama3_attn_head(layers[layer_id], attn_id, collect_stats=False)
 
-        lossdiff = loss2 - loss1   ## we want those where the loss increases after ablation;
+    obj = []
+    #### collecting the mean ablation token-wise loss 
+    for i in tqdm(range(args.num_samples)):
+        prompt = ds["train"][i]["conversation"][0]["content"]
+        loss = get_loss(model, tokenizer, prompt)
+        obj.append({"prompt": prompt, "loss": loss.cpu()})
+    torch.save(obj, os.path.join(args.savedir, f"mean_ablate_{args.head_str}.pt"))
 
 if __name__ == '__main__':
     main()
