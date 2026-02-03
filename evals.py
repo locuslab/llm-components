f52b8d5e1bd3d6a3927b21436345bafeea64d376
Anna Bair <annaebair@gmail.com>
2026-02-03 14:21:16 -0500

    Modify evals file

--- evals.py
+++ evals.py
@@ -72,11 +72,9 @@ def get_samples(task, num_samples):
                     samples = random.sample(range(total_examples), num_samples)
                     sampled_examples[subsubtask_key] = samples
     elif 'hellaswag_' in task:
-        # print(tm.load_task_or_group(task))
         total_examples = len(tm.load_task_or_group(task)[task].dataset['val'])
         sampled_examples = {task: random.sample(range(total_examples), num_samples)} 
     else:
-        # import pdb; pdb.set_trace()
         total_examples = len(tm.load_task_or_group(task)[task].dataset['test'])
         sampled_examples = {task: random.sample(range(total_examples), num_samples)}
     return sampled_examples
@@ -95,7 +93,6 @@ def lmeval_evaluate(model, task, sampled_examples):
         confirm_run_unsafe_code=True,
         samples=sampled_examples,
     )
-    # print(res)
     return extract_metric(task, res)
 
 
@@ -146,15 +143,12 @@ def swearing_eval(model, tokenizer, num_samples):
             decoded_strs = decoded_strs[0].partition("\nassistant\n")[-1]
         else:
             decoded_strs = decoded_strs[0].partition("assistant\n\n")[-1]
-        # import pdb; pdb.set_trace()
-        # print(decoded_strs)
         decoded_strs = decoded_strs.split()
         decoded_strs = [re.sub(r'\*\*(\w+)\*\*', r'\1', i) for i in decoded_strs] # Asterixes for bolding in markdown
         decoded_strs = [re.sub(r'[:.,"?()\n\t]', '', i).lower() for i in decoded_strs] # Punctuation marks
         generated_swear_words = [i for i in decoded_strs if i in swear_words]
         swear_count += len(generated_swear_words)
         for word in generated_swear_words:
-            # import pdb; pdb.set_trace()
             if len(df[df['text'].str.lower() == word]) > 0:
                 severity = df[df['text'].str.lower() == word]['severity_rating'].iloc[0]
                 badness_count += severity
@@ -186,100 +180,6 @@ def rhymes(w1, w2):
 
 def rhyming_eval(model, tokenizer, num_samples):
     rhyme_count = 0
-#     input_strs = [
-#         "door", "floor", "choir", "wire", "move", "groove",
-#         "tough", "stuff", "bowl", "soul", "rough", "muff",
-#         "sew", "go", "shoe", "blue", "bough", "now",
-#         "sure", "cure", "touch", "much", "pour", "more", "could", "should",
-#         "ghost", "most", "thought", "caught", "wolf", "gulf",
-#         "done", "run", "watch", "notch", "move", "prove",
-#         "gone", "dawn", "height", "kite", "queue", "chew",
-#         "yacht", "caught", "plough", "cow", "faux", "go",
-#         "jeans", "genes"
-# ]
-    # input_strs = [
-    #     "cat", "hat", "bat", "mat",
-    #     "sun", "run", "fun", "bun",
-    #     "light", "night", "bright", "flight",
-    #     "day", "way", "play", "stay",
-    #     "stone", "bone", "tone", "phone",
-    #     "hill", "bill", "will", "still",
-    #     "car", "far", "star", "bar",
-    #     "cold", "bold", "told", "gold",
-    #     "tree", "free", "three", "bee",
-    #     "chair", "fair", "pair", "stare",
-    #     "hand", "land", "sand", "band",
-    #     "ball", "fall", "tall", "call",
-    #     "ear", "near", "clear", "dear",
-    #     "red", "bed", "said", "led",
-    #     "heat", "seat", "beat", "street",
-    #     "coat", "boat", "goat", "float",
-    #     "ring", "sing", "king", "wing",
-    #     "sky", "fly", "cry", "try",
-    #     "tight", "sight", "might", "fight",
-    #     "rope", "hope", "soap", "scope",
-
-    #     # more challenging additions
-    #     "eclipse",          # rhymes with "ellipse"
-    #     "circuit",          # rhymes with "work it" (slant rhyme)
-    #     "serene",           # rhymes with "marine"
-    #     "oblige",           # rhymes with "s oblige"
-    #     "endeavor",         # rhymes with "never" / "clever" (near rhyme)
-    #     "arcane",           # rhymes with "domain"
-    #     "mirage",           # rhymes with "garage" (second syllable rhyming)
-    #     "adore",            # rhymes with "before"
-    #     "cuisine",          # rhymes with "marine"
-    #     "relief",           # rhymes with "belief"
-    #     "endeared",         # rhymes with "appeared"
-    #     "opaque"            # rhymes with "awake"
-    # ]
-
-    # input_strs += [
-    #     "break", "stake", "lose", "choose", "bread", "said",
-    #     "great", "straight", "warn", "torn", "read", "red",
-    #     "laugh", "calf", "steam", "scheme", "heart", "part",
-    #     "head", "spread", "ought", "bought", "chief", "belief",
-    #     "weird", "beard", "drought", "sprout", "heal", "feel",
-    #     "sleight", "flight", "rear", "fear", "spoke", "cloak",
-    #     "seize", "freeze", "steak", "shake", "bury", "ferry", 
-    #     "through", "threw", "proud", "cloud", "heighten", "frighten", 
-    #     "loose", "moose", "fire", "lyre", "piece", "peace", "scene", "seen"
-    # ]
-    # input_strs += [
-    #     "pint", "mint",
-    #     "cough", "off",
-    #     "though", "low",
-    #     "through", "drew",
-    #     "bough", "how",
-    #     "tough", "cuff",
-    #     "dough", "bo",
-    #     "lose", "bruise",
-    #     "sew", "though",
-    #     "blood", "flood",
-    #     "comb", "home",
-    #     "move", "prove",
-    #     "bury", "ferry",
-    #     "lead", "led",
-    #     "read", "red",
-    #     "wind", "find",
-    #     "have", "gave",
-    #     "love", "of",
-    #     "one", "won",
-    #     "height", "bite"
-    # ]
-    # new_words = [
-    #     "plough", "tough", "cough", "bough", "height", "weight", "done", "gone",
-    #     "love", "move", "comb", "tomb", "blood", "mood", "break", "streak",
-    #     "heard", "beard", "great", "sweat", "touch", "pouch", "rough", "through",
-    #     "sew", "few", "bury", "fury", "busy", "dizzy", "women", "linen",
-    #     "chief", "mischief", "cove", "pour", "sour", "shoe", "toe",
-    #     "though", "bought", "boot", "ought", "out", "wind", "mind",
-    #     "lose", "close", "gross", "dose", "host", "lost", "woes", "shoes",
-    #     "rove", "prove", "grove", "bleak", "steak", "speak", "bear", "fear",
-    #     "pear", "gear", "read", "bread", "tear", "year", "sewer",
-    #     "fewer", "lower", "bald", "scald", "vault", "halt",
-    #     "borough", "dough", "yacht", "caught"
-    # ]
     train_rhymes = ['dawn', 'caught', 'dose', 'cough', 'gulf', 'out', 'bought', 
                     'speak', 'chew', 'wire', 'go', 'steak', 'more', 'rove', 
                     'much', 'muff', 'bear', 'host', 'soul', 'pouch', 'gear', 
@@ -327,124 +227,3 @@ def rhyming_eval(model, tokenizer, num_samples):
     # print(round(rhyme_count / len(words), 4))
     return rhyme_count / len(words) 
 
-def counting_eval(model, tokenizer):
-
-    swear_count = 0
-    input_strs = [
-        # "Count by nines from 53 until 100",
-        "Count to 21 by threes",
-        "Count by fives to 25",
-        "Count by sixes until 38",
-        "Count by eights to 40",
-        "Count by 3s until 21",
-        "Count by 4s to 24",
-        "Count by twos to 18",
-        "Count by threes until 27",
-        "Count by fours to 32",
-        "Count by fives until 40",
-        "Count by sixes to 36",
-        "Count by sevens until 49",
-        "Count by 8s until 40",
-        "Count to 45 by 9s",
-        "Count by tens until 50",
-        "Count by elevens to 44",
-        "Count by twelves to 36",
-        "Count by 3s to 21",
-        "Count to 28 by 4s",
-        "Count by 30s to 180",
-        "Count by 35s to 140",
-        "Count by 40s to 200",
-        "Count by 50s to 250",
-        "Count by tens to 60",
-        "Count by elevens to 55",
-        "Count by 13s to 91",
-        "Count by 17s to 119",
-        "Count by 19s to 95",
-        "Count by 21s to 147",
-        "Count by 2s to 52"
-    ]
-    answers = [
-        # [53, 62, 71, 80, 89, 98],
-        [3, 6, 9, 12, 15, 18, 21],
-        [5, 10, 15, 20, 25],
-        [6, 12, 18, 24, 30, 36],
-        [8, 16, 24, 32, 40],
-        [3, 6, 9, 12, 15, 18, 21],
-        [4, 8, 12, 16, 20, 24],
-        [2, 4, 6, 8, 10, 12, 14, 16, 18],
-        [3, 6, 9, 12, 15, 18, 21, 24, 27],
-        [4, 8, 12, 16, 20, 24, 28, 32],
-        [5, 10, 15, 20, 25, 30, 35, 40],
-        [6, 12, 18, 24, 30, 36],
-        [7, 14, 21, 28, 35, 42, 49],
-        [8, 16, 24, 32, 40],
-        [9, 18, 27, 36, 45],
-        [10, 20, 30, 40, 50],
-        [11, 22, 33, 44],
-        [12, 24, 36],
-        [3, 6, 9, 12, 15, 18, 21],
-        [4, 8, 12, 16, 20, 24, 28],
-        [30, 60, 90, 120, 150, 180],
-        [35, 70, 105, 140],
-        [40, 80, 120, 160, 200],
-        [50, 100, 150, 200, 250],
-        [10, 20, 30, 40, 50, 60],
-        [11, 22, 33, 44, 55],
-        [13, 26, 39, 52, 65, 78, 91],
-        [17, 34, 51, 68, 85, 102, 119],
-        [19, 38, 57, 76, 95],
-        [21, 42, 63, 84, 105, 126, 147],
-        [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52]
-    ]
-    input_strs = [
-        "Count to 10",
-        "Count by 2s to 20",
-        "Count by 3s to 30.",
-        "Count by fours to forty",
-        "Count by fives to fifty",
-        "Count by 6s to 60",
-        "Count by 7s to 70",
-        "Count by 8s to 80",
-        "Count by nines to ninety",
-        "Count by tens to 100.",
-        "Count by elevens to 111",
-        "Count by 12s to 120",
-        "Count by 13s to 130s"
-    ]
-    answers = [
-        list(range(1, 11)),
-        list(range(2, 21, 2)),
-        list(range(3, 31, 3)), 
-        list(range(4, 41, 4)),
-        list(range(5, 51, 5)), 
-        list(range(6, 61, 6)), 
-        list(range(7, 71, 7)), 
-        list(range(8, 81, 8)), 
-        list(range(9, 91, 9)),
-        list(range(10, 101, 10)),
-        list(range(11, 112, 11)),
-        list(range(12, 121, 12)),  
-        list(range(13, 131, 13)) 
-]
-    correct = 0
-    for idx, input_str in enumerate(input_strs):
-        message_list = [{"role": "user", "content": "Respond with just the numerical answer: " + input_str}]
-        texts = tokenizer.apply_chat_template(message_list, tokenize=False, add_generation_prompt=True)
-        inputs = tokenizer(texts, padding="longest", padding_side="left", return_tensors="pt")
-        inputs = {key: val.to(model.device) for key, val in inputs.items()}
-        num_new_tokens = 512
-        with torch.no_grad():
-            outputs = model.generate(**inputs, do_sample=False, max_new_tokens=num_new_tokens, temperature=1.0, top_p=1.0)
-        decoded_strs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        
-        decoded_strs = decoded_strs[0].partition("assistant\n\n")[-1].split()
-        decoded_strs = [int(i.replace(',', '').replace('.', '')) for i in decoded_strs]
-        if decoded_strs[0] == 0:
-            del decoded_strs[0]
-        print(decoded_strs)
-        print(answers[idx])
-        print('\n')
-        if decoded_strs == answers[idx]:
-            correct += 1
-    print(correct / len(answers))
-    return correct / len(answers)
